48307985d347631d8c0f2cb93a851c91c42d9169
brenda acosta <luischang07@outlook.com>
2024-03-13 11:22:11 -0700

    Rama2

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    
+    @yield('content')
+    
+</body>
+</html>
