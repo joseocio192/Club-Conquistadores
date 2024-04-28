bbb248f735b5a7a2f21b0794e654b1cf0b2c859a
gatoguapo <victor._espinoza@hotmail.com>
2024-04-27 18:01:46 -0700

    SidebarData.jsx ejemplos agregados

--- resources/js/components/SidebarData.jsx
+++ resources/js/components/SidebarData.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import HomeIcon from '@mui/icons-material/Home';
+import PestControlRodentIcon from '@mui/icons-material/PestControlRodent';
 
 export const SidebarData = [
     {
-        title: "Ejemplo",
-        icon: <Icon></Icon>,
-        link: "/home"
-    }
+        title: "Ejemplo 1",
+        icon: <HomeIcon/>,
+        link: "/ejemplo1"
+    },
+    {
+        title: "Ejemplo 2",
+        icon: <PestControlRodentIcon/>,
+        link: "/ejemplo2"
+    },
+    {
+        title: "Ejemplo 3",
+        icon: <PestControlRodentIcon/>,
+        link: "/ejemplo3"
+    },
+    {
+        title: "Ejemplo 4",
+        icon: <PestControlRodentIcon/>,
+        link: "/ejemplo4"
+    },
+    {
+        title: "Ejemplo 5",
+        icon: <PestControlRodentIcon/>,
+        link: "/ejemplo5"
+    },
 ] 
