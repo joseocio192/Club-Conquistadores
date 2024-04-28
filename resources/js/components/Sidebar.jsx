dec85f7f09194865f8a287fb94e575902585f402
gatoguapo <victor._espinoza@hotmail.com>
2024-04-28 15:51:19 -0700

    Sidebar.jsx SidebarData implementada dentro de la intefaz

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -5,7 +5,16 @@ import { SidebarData } from './SidebarData';
 function Sidebar() {
     return (
         <div className='Sidebar'>
-           
+            <ul>
+                {SidebarData.map((val, key) => {
+                    return (
+                        <li key={key} onClick={() => {window.location.pathname = val.link}}> 
+                            <div>{val.icon}</div>
+                            <div>{val.title}</div> 
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
