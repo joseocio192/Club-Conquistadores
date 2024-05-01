6fd58d22fb47633be9332073c37dfea719c1811f
gatoguapo <victor._espinoza@hotmail.com>
2024-05-01 02:53:54 -0700

    SidebarData.jsx Alumnos data

--- resources/js/components/SidebarData.jsx
+++ resources/js/components/SidebarData.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import HomeIcon from '@mui/icons-material/Home';
+import ListIcon from '@mui/icons-material/List';
 import SchoolIcon from '@mui/icons-material/School';
 
 export const SidebarDataInstructores = [
     {
         title: "Cursos",
+        icon: <ListIcon/>,
+        link: "/cursos",
+    },
+    {
+        title: "Alumnos",
         icon: <SchoolIcon/>,
-        link: "/cursos"
+        link: "/alumnos",
     }
 ] 
