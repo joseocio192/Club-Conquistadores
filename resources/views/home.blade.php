cd04a5b75b913b1983cbd686e251485c4e89ee0a
Jose Pablo Ocio <joseociom@outlook.com>
2024-05-03 14:51:28 -0700

    Creada la vista vw_ciudadLocale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Club de Conquistadores</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Club de Conquistadores</title>
 
 </head>
 <body>
-    <h1>Club de Conquistadores</h1>
-    <p>¡Bienvenido!</p>
-    <p>Por favor, inicia sesión o regístrate para continuar.</p>
-    <form action="{{ route('login') }}" method="GET">
-        <button type="submit">Iniciar sesión</button>
-    </form>
-    <form action="{{ route('register') }}" method="GET">
-        <button type="submit">Registrarse</button>
-    </form>
-</body>
+     <h1>Club de Conquistadores</h1>
+     <p>¡Bienvenido!</p>
+     <p>Por favor, inicia sesión o regístrate para continuar.</p>
+     <form action="{{ route('login') }}" method="GET">
+          <button type="submit">Iniciar sesión</button>
+     </form>
+     <form action="{{ route('register') }}" method="GET">
+          <button type="submit">Registrarse</button>
+     </form>
+     @if($records->count())
+         <h2>Records:</h2>
+         <ul>
+        @foreach($records as $record)
+            <li>{{ $record->nombre . " " . $record->locale }}</li> <!-- Replace 'column1' with the actual column name -->
+        @endforeach
+         </ul>
+     @else
+         <p>No records found.</p>
+     @endif
 </body>
 </html>
