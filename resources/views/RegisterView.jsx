fc57eb9df0a6a1ba0af658583aab19e2e7ea22bd
gatoguapo <victor._espinoza@hotmail.com>
2024-05-04 01:50:48 -0700

    RegisterView.jsx function RegisterView creada

---
+++ resources/views/RegisterView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import "../../css/Sidebar.css"
+
+function RegisterView() {
+    return (
+        <div className='Register'>
+            
+        </div>
+    );
+}
+
+export default RegisterView
