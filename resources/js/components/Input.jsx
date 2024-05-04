c8b6eddc4bb4a88878bf0240bee7854c92b76f20
gatoguapo <victor._espinoza@hotmail.com>
2024-05-04 01:46:20 -0700

    Input.jsx function Input (text, type, classname)

--- resources/js/components/Input.jsx
+++ resources/js/components/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 //import "../../css/Sidebar.css"
 
-function Input() {
+function Input(text, type, className) {
     return (
         <label>
-            <input></input>
+            <input type={type} className={className}></input>
+            {text}
         </label>
     );
 }
