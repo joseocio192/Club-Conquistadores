1fac02ac0e6acd2919aa5662cba15e964442a37c
gatoguapo <victor._espinoza@hotmail.com>
2024-05-04 01:39:10 -0700

    Input.jsx function Input creada

---
+++ resources/js/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import "../../css/Sidebar.css"
+
+function Input() {
+    return (
+        <label>
+            <input></input>
+        </label>
+    );
+}
+
+export default Input
