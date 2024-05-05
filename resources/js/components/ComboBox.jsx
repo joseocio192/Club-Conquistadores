8cf4aea0bbdd938c1a8508e53c5b72829560fa65
gatoguapo <victor._espinoza@hotmail.com>
2024-05-04 20:05:53 -0700

    ComboBox.jsx funcion

--- resources/js/components/ComboBox.jsx
+++ resources/js/components/ComboBox.jsx
@@ -1,3 +1,15 @@
 import React from 'react'
 import '../../css/ComboBox.css'
 
+export function ComboBox({text, classNameLabel ,classNameCombo}) {
+    return (
+        <div className='LayoutCombo'>
+           <label className={classNameLabel}>
+           {text}
+            </label>  
+            <select
+                className={classNameCombo}>
+            </select>
+        </div>   
+    )
+}
