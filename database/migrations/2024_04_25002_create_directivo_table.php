c9145f8cf573ed6e8360ccac20baa7c182b1cd46
Jose Pablo Ocio <joseociom@outlook.com>
2024-05-05 20:56:24 -0700

    por alguna razon el jefe no puede ser nulo y tienes que meterte a la base de datos a cambiarlo

--- database/migrations/2024_04_25002_create_directivo_table.php
+++ database/migrations/2024_04_25002_create_directivo_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('Directivo', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('Users');
+            //por alguna razon el jefe no puede ser nulo y tienes que meterte a la base de datos a cambiarlo
             $table->foreignId('jefe_id')->references('id')->on('Directivo')->nullable();
             $table->foreignID('ciudad_id')->references('id')->on('Ciudad');
             //Esto no puede ser una restriccion estatica Esto debe ser una restriccion dinamica por el idioma
