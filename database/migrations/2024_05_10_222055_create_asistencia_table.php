5df04c480fb6050543a1b22323b9ba253b38b6fe
Jose Pablo Ocio <joseociom@outlook.com>
2024-05-22 15:01:22 -0700

    fixed dates on asistencia

--- database/migrations/2024_05_10_222055_create_asistencia_table.php
+++ database/migrations/2024_05_10_222055_create_asistencia_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('Asistencia', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_clase')->references('id')->on('Clase');
+            $table->date('fecha');
             $table->timestamps();
         });
     }
