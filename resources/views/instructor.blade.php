f74c7a599718c381203b3a6a85f0739e80cde8ad
gatoguapo <victor._espinoza@hotmail.com>
2024-05-25 22:34:53 -0700

    vista instructor tabla de asistencias puesta en vertical de nuevo y con scroll vertical

--- resources/views/instructor.blade.php
+++ resources/views/instructor.blade.php
@@ -109,67 +109,68 @@
                     <form action="{{ route('instructor.definer') }}" method="post">
                         @csrf
                         <input type="text" name="clase_id" value="{{ $clase->id }}" style="display: none;">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Fecha</th>
-                                    <th>Asistencia</th>
-                                    <th> Nivel de Pulcritud</th>
-                                    <th class="thBtnDia">
-                                        <button class="btnDia" type="submit" name="adddia">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                                <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                                <path d="M416 208H272V64c0-17.7-14.3-32-32-32h-32c-17.7 0-32 14.3-32 32v144H32c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h144v144c0 17.7 14.3 32 32 32h32c17.7 0 32-14.3 32-32V304h144c17.7 0 32-14.3 32-32v-32c0-17.7-14.3-32-32-32z"/>
-                                            </svg>
-                                        </button>
-                                        <button class="btnDia" type="submit" name="deleteDia">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                                <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                                <path d="M416 208H32c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h384c17.7 0 32-14.3 32-32v-32c0-17.7-14.3-32-32-32z"/>
-                                            </svg>
-                                        </button>
-                                    </th>
-                                </tr>
-                            </thead>
-                            @foreach ($conquistadores as $conquistador)
-                                <tr>
-                                    @if ($asistencias->count() == 0)
-                                        <th>No hay asistencias</th>
-                                    @else                      
-                                        @foreach ($conquistador->asistencia as $asistencia)
-                                            @if ($asistencia->id_clase === $clase->id)
-                                                <tr>
-                                                    <td>{{ $conquistador->user->name }}</td>
-                                                    <th>{{ $asistencia->fecha }}</th>
-                                                    <td>
-                                                        <input type="checkbox"
-                                                            name="asistencia_{{ $asistencia->pivot->id_asistencia . '-' . $asistencia->pivot->id_conquistador }}"
-                                                            value="1" @if ($asistencia->pivot->asistio == 1) checked @endif
-                                                        >
-                                                    </td>
-                                                    <td>
-                                                        <select class="selectPulcritud"
-                                                            name="pulcritud_{{ $asistencia->pivot->id_asistencia . '-' . $asistencia->pivot->id_conquistador }}">
-                                                            <option value="1"
-                                                                @if ($asistencia->pivot->pulcritud == 1) selected @endif>1</option>
-                                                            <option value="2"
-                                                                @if ($asistencia->pivot->pulcritud == 2) selected @endif>2</option>
-                                                            <option value="3"
-                                                                @if ($asistencia->pivot->pulcritud == 3) selected @endif>3</option>
-                                                            <option value="4"
-                                                                @if ($asistencia->pivot->pulcritud == 4) selected @endif>4</option>
-                                                            <option value="5"
-                                                                @if ($asistencia->pivot->pulcritud == 5) selected @endif>5</option>
-                                                        </select>
-                                                    </td>
-                                                </tr>
-                                            @endif
-                                        @endforeach
-                                    @endif
-                                </tr>
-                            @endforeach
-                        </table>
+                        <div class="divTablaAsis">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        @if ($asistencias->count() == 0)
+                                            <th>No hay asistencias</th>
+                                        @else
+                                            @foreach ($conquistador->asistencia as $asistencia)
+                                            <th>{{ $asistencia->fecha }}</th>
+                                            @endforeach
+                                        @endif
+                                        <th class="thBtnDia">
+                                            <button class="btnDia" type="submit" name="adddia">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                    <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                                    <path d="M416 208H272V64c0-17.7-14.3-32-32-32h-32c-17.7 0-32 14.3-32 32v144H32c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h144v144c0 17.7 14.3 32 32 32h32c17.7 0 32-14.3 32-32V304h144c17.7 0 32-14.3 32-32v-32c0-17.7-14.3-32-32-32z"/>
+                                                </svg>
+                                            </button>
+                                            <button class="btnDia" type="submit" name="deleteDia">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                    <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                                    <path d="M416 208H32c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h384c17.7 0 32-14.3 32-32v-32c0-17.7-14.3-32-32-32z"/>
+                                                </svg>
+                                            </button>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                @foreach ($conquistadores as $conquistador)
+                                    <tr>
+                                        @if ($asistencias->count() == 0)
+                                            <th>No hay asistencias</th>
+                                        @else       
+                                            <td>{{ $conquistador->user->name }}</td>                 
+                                            @foreach ($conquistador->asistencia as $asistencia)
+                                                @if ($asistencia->id_clase === $clase->id)            
+                                                        <td class="tdAsistencia">
+                                                            <input type="checkbox"
+                                                                name="asistencia_{{ $asistencia->pivot->id_asistencia . '-' . $asistencia->pivot->id_conquistador }}"
+                                                                value="1" @if ($asistencia->pivot->asistio == 1) checked @endif/>
+                                                            <select class="selectPulcritud"
+                                                                name="pulcritud_{{ $asistencia->pivot->id_asistencia . '-' . $asistencia->pivot->id_conquistador }}">
+                                                                <option value="1"
+                                                                    @if ($asistencia->pivot->pulcritud == 1) selected @endif>1</option>
+                                                                <option value="2"
+                                                                    @if ($asistencia->pivot->pulcritud == 2) selected @endif>2</option>
+                                                                <option value="3"
+                                                                    @if ($asistencia->pivot->pulcritud == 3) selected @endif>3</option>
+                                                                <option value="4"
+                                                                    @if ($asistencia->pivot->pulcritud == 4) selected @endif>4</option>
+                                                                <option value="5"
+                                                                    @if ($asistencia->pivot->pulcritud == 5) selected @endif>5</option>
+                                                            </select>
+                                                        </td>
+                                                @endif
+                                            @endforeach
+                                        @endif
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                        
                         <button class="btnEnviar" type="submit" name="save">Enviar</button>
                     </form>
 
