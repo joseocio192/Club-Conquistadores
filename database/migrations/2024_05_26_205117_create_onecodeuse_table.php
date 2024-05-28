d1bd2f293b65f89b8c11793af1a13b10381fb6a7
Jose Pablo Ocio <joseociom@outlook.com>
2024-05-28 09:59:58 -0700

    fix fecha nacimiento #64

---
+++ database/migrations/2024_05_26_205117_create_onecodeuse_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('onecodeuse', function (Blueprint $table) {
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('onecode');
+            $table->boolean('used');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('onecodeuse');
+    }
+};
