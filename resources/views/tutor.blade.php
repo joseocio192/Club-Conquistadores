cc529e327d9dc1a433c82299dd7217e14f6701b0
gatoguapo <victor._espinoza@hotmail.com>
2024-05-27 19:40:46 -0700

    vista tutor card datos del pupilo

--- resources/views/tutor.blade.php
+++ resources/views/tutor.blade.php
@@ -24,7 +24,13 @@
             </svg>
             Añadir pupilo</a>
         @foreach ($pupilos as $pupilo)
-            <a href="{{route('tutor.show', $pupilo->id)}}">{{$pupilo->user->name}}</a>
+            <a href="{{route('tutor.show', $pupilo->id)}}">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path d="M319.4 320.6L224 416l-95.4-95.4C57.1 323.7 0 382.2 0 454.4v9.6c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-9.6c0-72.2-57.1-130.7-128.6-133.8zM13.6 79.8l6.4 1.5v58.4c-7 4.2-12 11.5-12 20.3 0 8.4 4.6 15.4 11.1 19.7L3.5 242c-1.7 6.9 2.1 14 7.6 14h41.8c5.5 0 9.3-7.1 7.6-14l-15.6-62.3C51.4 175.4 56 168.4 56 160c0-8.8-5-16.1-12-20.3V87.1l66 15.9c-8.6 17.2-14 36.4-14 57 0 70.7 57.3 128 128 128s128-57.3 128-128c0-20.6-5.3-39.8-14-57l96.3-23.2c18.2-4.4 18.2-27.1 0-31.5l-190.4-46c-13-3.1-26.7-3.1-39.7 0L13.6 48.2c-18.1 4.4-18.1 27.2 0 31.6z"/>
+                </svg>
+                {{$pupilo->user->name}}
+            </a>
         @endforeach
         <form class="formLogOut" action="/logout" method="get">
             @csrf
@@ -52,9 +58,28 @@
             </div>
             @endif
             @if ($status == 'show')
-            <h2>sus datos:</h2>
-            <h3>Id: {{$hijo->user->name}}</h3>
-            <h3>Nombre: {{$hijo->user->name}} {{$hijo->user->apellido}}</h3>
+            <div class="divDatos">
+                <h2>Datos del pupilo:</h2>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" >
+                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
+                    </svg>
+                    <h3>Id: {{$hijo->user->id}}</h3>
+                    <h3>Nombre: {{$hijo->user->name}} {{$hijo->user->apellido}}</h3>
+                    <h3>Edad: {{$hijo->user->edad}}</h3>
+                </div>
+                <h3>Telefono: {{$hijo->user->telefono}}</h3>
+                <h3>Fecha de nacimiento: {{$hijo->user->fecha_nacimiento}}</h3>
+                <h3>Direccion:
+                    {{ $hijo->user->colonia . ' ' . 
+                    $hijo->user->calle . ' ' . 
+                    $hijo->user->numero_exterior }}
+                </h3>
+            </div>
+            
+            
+            
             @endif
         </ul>
     </div>
