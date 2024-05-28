fa8019cb38f8f7e4abe12d377558a4688f97194d
gatoguapo <victor._espinoza@hotmail.com>
2024-05-28 16:04:15 -0700

    cambios sutiles de diseño en login y register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,6 +2,9 @@
 <!DOCTYPE html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link href="{{ asset('/css/register.css') }}" rel="stylesheet">
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style>
 <form class="FormLayout" method="POST" action="{{ route('register') }}" id="registro">
     @csrf
 
